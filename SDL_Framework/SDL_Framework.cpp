cfc21b80e654883b7a144ed14174ae79be6ae091
Calem Harris <melacsirrah575@gmail.com>
2024-11-04 22:56:48 -0400

    D1 Code Along

--- SDL_Framework/SDL_Framework.cpp
+++ SDL_Framework/SDL_Framework.cpp
@@ -1,20 +1,18 @@
-// SDL_Framework.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int argc, char* args[])
 {
-    std::cout << "Hello World!\n";
+    //Initialize SDL subsytems
+    if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0) {
+        //We have failed to initialize
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    //TODO: DRAW A WINDOW
+
+    //Terminate SDL subsystems
+    SDL_Quit();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
