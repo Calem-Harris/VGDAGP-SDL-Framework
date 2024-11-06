0f4d5079edb9d9d0aff007a9f923816f80dfe73e
Calem Harris <melacsirrah575@gmail.com>
2024-11-05 23:06:52 -0400

    Day 2 Missed Changes

--- SDL_Framework/SDL_Framework.cpp
+++ SDL_Framework/SDL_Framework.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <SDL.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-SDL_Window* window = nullptr;
-SDL_Renderer* renderer = nullptr;
 bool quit = false;
 
 int main(int argc, char* args[])
