5899c5cd67ecaf5ec55583713ebcd1a2f2223533
Calem Harris <melacsirrah575@gmail.com>
2024-11-18 22:29:23 -0400

    Created base Collider class and Box/Circle Colliders. Also created a PhysEntity class that has a couple more requirements to it before we can finish it.

---
+++ SDL_Framework/PhysEntity.cpp
@@ -0,0 +1,6 @@
+#include "PhysEntity.h"
+//TODO:
+
+
+//Need PhysicsManager to be able to determine when colliders have intersected with each other
+//Might need some extra bits of functionality (aka a helper)
