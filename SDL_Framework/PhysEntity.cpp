ffadc7ab5ff219076b846fc3edb4d76c799c992e
Calem Harris <melacsirrah575@gmail.com>
2024-11-19 22:30:45 -0400

    Created PhysicsManager class. Refactored MathHelper and started working on a PhysicsHelper file.

--- SDL_Framework/PhysEntity.cpp
+++ SDL_Framework/PhysEntity.cpp
@@ -1,6 +1,3 @@
 #include "PhysEntity.h"
 //TODO:
-
-
-//Need PhysicsManager to be able to determine when colliders have intersected with each other
 //Might need some extra bits of functionality (aka a helper)
